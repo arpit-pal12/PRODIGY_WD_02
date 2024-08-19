400a2205f215c354bc828c4a8ce26347821d1bea
Arpit Pal <arpitpal552004@gmail.com>
2024-08-19 07:56:31 +0530

    Create restaurent.js

---
+++ restaurent.js
@@ -0,0 +1,77 @@
+export const restaurant = [
+
+    {
+        decs : "Bakeries near me",
+        link : "#",
+    },
+    {
+        decs : "Bars near me",
+        link : "#",
+    },
+    {
+        decs : "Beverage Shops near me",
+        link : "#",
+    },
+    {
+        decs : "Bhojanalya near me",
+        link : "#",
+    },
+    {
+        decs : "Cafés near me",
+        link : "#",
+    },
+    {
+        decs : "Casual Dining near me",
+        link : "#",
+    },
+    {
+        decs : "Cocktail Bars near me",
+        link : "#",
+    },
+    {
+        decs : "Confectioneries near me",
+        link : "#",
+    },
+    {
+        decs : "Dessert Parlors near me",
+        link : "#",
+    },
+    {
+        decs : "Dhabas near me",
+        link : "#",
+    },
+    {
+        decs : "Fine Dining near me",
+        link : "#",
+    },
+    {
+        decs : "Food Courts near me",
+        link : "#",
+    },
+    {
+        decs : "Food Trucks near me",
+        link : "#",
+    },
+    {
+        decs : "Kiosks near me",
+        link : "#",
+    },
+    {
+        decs : "Lounges near me",
+        link : "#",
+    },
+    {
+        decs : "Microbreweries near me",
+        link : "#",
+    },
+    {
+        decs : "Quick Bites near me",
+        link : "#",
+    },
+    {
+        decs : "Sweet Shops near me",
+        link : "#",
+    },
+
+
+]
