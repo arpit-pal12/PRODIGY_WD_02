9c5416614f3e942187292b1fb307bcd31f71562f
Arpit Pal <arpitpal552004@gmail.com>
2024-08-19 07:55:52 +0530

    popularCity.js

---
+++ popularCity.js
@@ -0,0 +1,89 @@
+export const popularcity = [
+    {
+        decs : "Bakery food near me",
+        link : "#",
+    },
+    {
+        decs : "Beverages food near me",
+        link : "#",
+    },
+    {
+        decs : "Biryani food near me",
+        link : "#",
+    },
+    {
+        decs : "Burger food near me",
+        link : "#",
+    },
+    {
+        decs : "Chinese food near me",
+        link : "#",
+    },
+    {
+        decs : "Continental food near me",
+        link : "#",
+    },
+    {
+        decs : "Desserts food near me",
+        link : "#",
+    },
+    {
+        decs : "Ice Cream food near me",
+        link : "#",
+    },
+    {
+        decs : "Mithai food near me",
+        link : "#",
+    },
+    {
+        decs : "Momos food near me",
+        link : "#",
+    },
+    {
+        decs : "Mughlai food near me",
+        link : "#",
+    },
+    {
+        decs : "North Indian food near me",
+        link : "#",
+    },
+    {
+        decs : "Pasta food near me",
+        link : "#",
+    },
+    {
+        decs : "Pizza food near me",
+        link : "#",
+    },
+    {
+        decs : "Rolls food near me",
+        link : "#",
+    },
+    {
+        decs : "Sandwich food near me",
+        link : "#",
+    },
+    {
+        decs : "Shake food near me",
+        link : "#",
+    },
+    {
+        decs : "South Indian food near me",
+        link : "#",
+    },
+    {
+        decs : "Street food near me",
+        link : "#",
+    },
+    {
+        decs : "Tea food near me",
+        link : "#",
+    },
+
+
+
+
+
+    
+
+]
