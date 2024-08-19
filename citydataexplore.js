7d8668e6d5546bd59ba4d672ab7c14d34460f6dc
Arpit Pal <arpitpal552004@gmail.com>
2024-08-19 07:54:39 +0530

    citydataexplore.js

---
+++ citydataexplore.js
@@ -0,0 +1,111 @@
+ export const cityName =[
+    {
+        decs :"Delhi NCR",
+        
+    },
+    {
+        decs :"Kolkata",
+      
+    },
+    {
+        decs :"Mumbai",
+        
+    },
+    {
+        decs :"Bengaluru",
+        
+    },
+    {
+        decs :"Pune",
+        
+    },
+    {
+        decs :"Hyderabad",
+       
+    },
+    {
+       decs :"Chennai",
+        
+    },
+    {
+        decs :"Lucknow",
+        
+    },
+    {
+        decs :"Kochi",
+        
+    },
+    {
+        decs :"Jaipur",
+        
+    },
+    {
+        decs :"Ahemedabad",
+        
+    },
+    {
+        decs :"Chandigarh",
+        
+    },
+    {
+        decs :"Goa",
+        
+    },
+    {
+        decs :"Indore",
+       
+    },
+    {
+        decs :"Gangtok",
+    },
+    {
+        decs :"Nashik",
+       
+    },
+    {
+        decs :"Ooty",
+        
+    },
+    {
+        decs :"Shimla",
+       
+    },
+    {
+        decs :"Ludhiana",
+        
+    },
+    {
+        decs :"Amritsar",
+        
+    },
+    {
+        decs :"Kanpur",
+        
+    },
+    {
+        decs :"Allahabad",
+        
+    },
+    {
+        decs :"Aurangabad",
+        
+    },
+    {
+        decs :"Bhopal",
+        
+    },
+    {
+        decs :"Ranchi",
+        
+    },
+    {
+        decs :"Visakhapatnam",
+        
+    },
+    {
+        decs :"Bhubneshwar",
+        
+    },
+
+
+ ]
