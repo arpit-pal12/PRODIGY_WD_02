e83b5546593fd33dc9b6164d2b359b6bfb3bb93c
Arpit Pal <arpitpal552004@gmail.com>
2024-08-19 07:57:09 +0530

    Create topRestaurant.js

---
+++ topRestaurant.js
@@ -0,0 +1,30 @@
+export const topRestaurant = [
+    {
+        decs : "Bikanervala",
+        link : "#",
+    },
+    {
+        decs : "Burger King",
+        link : "#",
+    },
+    {
+        decs : "KFC",
+        link : "#",
+    },
+    {
+        decs : "McDonald's",
+        link : "#",
+    },
+    {
+        decs : "Pizza Hut",
+        link : "#",
+    },
+    {
+        decs : "Sagar Ratna",
+        link : "#",
+    },
+    {
+        decs : "WOW!Momo",
+        link : "#",
+    },
+]
