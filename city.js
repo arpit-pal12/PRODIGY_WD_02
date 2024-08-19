49b2f25e17341855532c506a0ac7780a9030c44c
Arpit Pal <arpitpal552004@gmail.com>
2024-08-19 07:53:36 +0530

    Create city.js

---
+++ city.js
@@ -0,0 +1,40 @@
+export const city = [
+    {
+        name :"Mall Road",
+        place :105,
+    },
+    {
+        name :"Swaroop Nagar",
+        place :161,
+    },
+    {
+        name :"Tilak Nagar",
+        place :48,
+    },
+    {
+        name :"Lajpat Nagar",
+        place :79,
+    },
+    {
+        name :"Ashok Nagar",
+        place :95,
+    },
+    {
+        name :"Pared",
+        place :52,
+    },
+    {
+        name :"Arya Nagar",
+        place :42,
+    },
+    {
+        name :"Kakadeo",
+        place :200,
+    },
+    {
+        name :"see more",
+        place : "",
+        icon : "fa-solid fa-angle-right",
+    },
+
+]
